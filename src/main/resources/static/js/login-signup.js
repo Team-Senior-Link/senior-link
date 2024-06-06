c9e37ca5c7b6abc2d7b90def14e522549a87ca66
SumiSong <mjsong2001@naver.com>
2024-06-06 12:22:24 +0900

    예약 디자인 수정

--- src/main/resources/static/js/login-signup.js
+++ src/main/resources/static/js/login-signup.js
@@ -1,28 +1,16 @@
-(function() {
+(function () {
     var signup = $('.container--static .button--signup');
     var login = $('.container--static .button--login');
     var signupContent = $('.container--sliding .slider-content.signup');
     var loginContent = $('.container--sliding .slider-content.login');
     var slider = $('.container--sliding');
 
-    signup.on('click', function() {
+    signup.on('click', function () {
         loginContent.css('display', 'none');
         signupContent.css('display', 'initial');
         slider.animate({
             'left': '30%'
         }, 350, 'easeOutBack');
-
-
-        function onRoleChange() {
-        var roleSelect = document.getElementById("role");
-        var gradeDiv = document.getElementById("gradeDiv");
-        if (roleSelect.value === "user") {
-        gradeDiv.style.display = "block";
-        } else {
-            gradeDiv.style.display = "none";
-        }
-        }
-
     });
 
     login.on('click', function () {
@@ -33,3 +21,25 @@
         }, 350, 'easeOutBack');
     });
 })();
+
+function toggleGradeSection() {
+    var roleSelect = document.getElementById('role');
+    var gradeSection = document.getElementById('grade-section');
+    if (roleSelect.value === 'CARERECEIVER') {
+        gradeSection.style.display = 'block';
+    } else {
+        gradeSection.style.display = 'none';
+    }
+}
+
+// DOM이 로드되었을 때 등급 섹션 토글
+document.addEventListener('DOMContentLoaded', function() {
+    toggleGradeSection();
+});
+
+function convertEmptyToNull() {
+    var gradeSelect = document.getElementById('grade');
+    if (gradeSelect.value === "") {
+        gradeSelect.value = null;
+    }
+}
