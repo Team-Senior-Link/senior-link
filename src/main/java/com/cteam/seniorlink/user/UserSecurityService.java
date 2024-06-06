e3da5a04ecf9b8bc6b33e57164ec90ff2b779e30
SumiSong <mjsong2001@naver.com>
2024-06-06 18:16:30 +0900

    예약 디자인 수정, 코드 병합

--- src/main/java/com/cteam/seniorlink/user/UserSecurityService.java
+++ src/main/java/com/cteam/seniorlink/user/UserSecurityService.java
@@ -1,15 +1,11 @@
 package com.cteam.seniorlink.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+
 
 @RequiredArgsConstructor
 @Service
@@ -17,12 +13,7 @@ public class UserSecurityService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRole().getValue())); // 역할 설정
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
